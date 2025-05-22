cd6c53f3016a4feb83256927282a5ab793519387
DSYS <dinodimanjith404@gmail.com>
2025-05-22 11:19:36 +0530

    Todo save API implemented

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,31 @@
 import React from "react";
 import { useState } from "react";
+import axios from "axios";
 
 const App = () => {
   const [newTodo, setNewTodo] = useState("");
+  const [todos, setTodos] = useState([]);
+  const addTodo = async () => {
+    if (!newTodo.trim()) return;
+    try {
+      const response = await axios.post("/api/todos/", { text: newTodo });
+      setNewTodo("");
+      setTodos(...todos, response.data);
+      console.log(response.data);
+    } catch (error) {
+      console.log("Error adding todo ", error);
+    }
+  };
   return (
     <div className="min-h-screen bg-gradient-to-br from gray-50 to-gray-800 flex items-center justify-center p-4">
       <div className="bg-white rounded-2xl shadow-xl w-full max-w-lg p-8">
         <h1 className="text-4xl font-bold text-gray-800 mb-8">Task Manager</h1>
         <form
           className="flex items-center gap-2 shadow-sm border border-gray-200 p-2 rounded-lg"
-          onSubmit={(e) => e.preventDefault()}
+          onSubmit={(e) => {
+            e.preventDefault();
+            addTodo();
+          }}
         >
           <input
             className="flex-1 outline-none px-3 py-2 text-gray-700 placeholder-gray-400"
@@ -19,7 +35,11 @@ const App = () => {
             placeholder="What needs to be done ?"
             required
           />
-          <button className="bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded-md font-medium cursor-pointer">
+          <button
+            type="submit"
+            className="bg-blue-500 hover:bg-blue-600
+           text-white px-4 py-2 rounded-md font-medium cursor-pointer"
+          >
             Add Task
           </button>
         </form>
@@ -27,5 +47,4 @@ const App = () => {
     </div>
   );
 };
-
 export default App;
