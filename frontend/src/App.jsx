a94dca20a4ea073698aa176e1f1d4de9971e3cfb
DSYS <dinodimanjith404@gmail.com>
2025-05-22 14:46:39 +0530

    implemented updated function and build UI for that

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,25 +1,72 @@
 import React from "react";
 import { useState } from "react";
 import axios from "axios";
+import { useEffect } from "react";
+import { MdModeEditOutline } from "react-icons/md";
+import { FaTrash } from "react-icons/fa";
+import { MdOutlineDone } from "react-icons/md";
+import { IoClose } from "react-icons/io5";
 
 const App = () => {
   const [newTodo, setNewTodo] = useState("");
   const [todos, setTodos] = useState([]);
+  const [editingToDo, setEditingToDo] = useState(null);
+  const [editedText, setEditedText] = useState("");
+
+  //add todo function
   const addTodo = async () => {
     if (!newTodo.trim()) return;
     try {
       const response = await axios.post("/api/todos/", { text: newTodo });
       setNewTodo("");
-      setTodos(...todos, response.data);
-      console.log(response.data);
+      setTodos([...todos, response.data]);
     } catch (error) {
       console.log("Error adding todo ", error);
     }
   };
+
+  //fetching all the todos
+  const fetchTodos = async () => {
+    try {
+      const response = await axios.get("/api/todos/");
+      setTodos(response.data);
+    } catch (error) {
+      console.log("Error adding todo ", error);
+    }
+  };
+
+  //USE effect
+  useEffect(() => {
+    fetchTodos();
+  }, []);
+
+  //editing methiod
+  const startediting = (todo) => {
+    setEditingToDo(todo._id);
+    setEditedText(todo.text);
+  };
+
+  //update todo in backend
+  const updateTodo = async (id) => {
+    try {
+      const response = await axios.patch(`api/todos/${id}`, {
+        text: editedText,
+      });
+      console.log(response.data);
+      setTodos(todos.map((todo) => (todo._id === id ? response.data : todo)));
+      setEditingToDo(null);
+    } catch (error) {
+      console.log("Error adding todo ", error);
+    }
+  };
+
+  //return
   return (
     <div className="min-h-screen bg-gradient-to-br from gray-50 to-gray-800 flex items-center justify-center p-4">
       <div className="bg-white rounded-2xl shadow-xl w-full max-w-lg p-8">
-        <h1 className="text-4xl font-bold text-gray-800 mb-8">Task Manager</h1>
+        <h1 className="text-4xl font-bold text-gray-800 mb-8 text-center">
+          Task Manager
+        </h1>
         <form
           className="flex items-center gap-2 shadow-sm border border-gray-200 p-2 rounded-lg"
           onSubmit={(e) => {
@@ -43,6 +90,72 @@ const App = () => {
             Add Task
           </button>
         </form>
+        <div className="mt-4">
+          {todos.length !== 0 ? (
+            <div className="flex flex-col gap-4">
+              {todos.map((todo) => (
+                <div key={todo._id}>
+                  {editingToDo === todo._id ? (
+                    <div className="flex items-center gap-x-3">
+                      <input
+                        className="flex-1 p-3 border rounded-lg border-gray-200 outline-none focus:ring-2 focus:ring-blue-300 text-gray-700 shadow-inner"
+                        type="text"
+                        value={editedText}
+                        onChange={(e) => setEditedText(e.target.value)}
+                      />
+                      <div className="flex gap-x-2">
+                        <button
+                          onClick={() => updateTodo(todo._id)}
+                          className="px-4 py-2 bg-green-500 text-white rounded-lg hover:bg-green-600 cursor-pointer"
+                        >
+                          <MdOutlineDone />
+                        </button>
+                        <button
+                          className="px-4 py-2 bg-gray-200 text-gray rounded-lg hover:bg-gray-300 cursor-pointer"
+                          onClick={(e) => setEditingToDo(null)}
+                        >
+                          <IoClose />
+                        </button>
+                      </div>
+                    </div>
+                  ) : (
+                    <div>
+                      <div className="flex items-center justify-between">
+                        <div className="flex items-center gap-x-4">
+                          <button
+                            className={`h-6 w-6 border rounded-full flex items-center justify-center ${
+                              todo.completed
+                                ? "bg-green-500 border-green-500"
+                                : "border-gray-300 hover:border-blue-400"
+                            }`}
+                          >
+                            {todo.completed && <MdOutlineDone />}
+                          </button>
+                          <span className="text-gray-800 font-medium">
+                            {todo.text}
+                          </span>
+                        </div>
+                        <div className="flex gap-x-2">
+                          <button
+                            className="p-2 text-blue-500 hover:text-blue-700 rounded-lg hover:bg-blue-50 duration-200"
+                            onClick={(e) => startediting(todo)}
+                          >
+                            <MdModeEditOutline />
+                          </button>
+                          <button className="p-2 text-red-500 hover:text-red-700 rounded-lg hover:bg-blue-50 duration-200">
+                            <FaTrash />
+                          </button>
+                        </div>
+                      </div>
+                    </div>
+                  )}
+                </div>
+              ))}
+            </div>
+          ) : (
+            <div>No Todos</div>
+          )}
+        </div>
       </div>
     </div>
   );
